7abb7eaa7fb568cdef1869e238200ea0e45f30a3
ellehcim burton <ellehcimburton@gmail.com>
2024-01-25 19:41:50 -0500

    feat: add solution to remove Kth Node from End

--- LeetcodeQuestions/firstDuplicateValue.js
+++ LeetcodeQuestions/firstDuplicateValue.js
@@ -10,3 +10,17 @@ function firstDuplicateValue(array) {
   }
   return -1;
 }
+
+// set up a hash to keep track of the values. 
+// iterate through the array
+// if the valuea at the current index is in the hash, return the value else add it to the hash. 
+// O(n) time | O(n) space
+
+
+function firstDuplicateValue(array) {
+    for (let i = 0; i < array.length; i++) {
+        let absValue = Math.abs(array[i]);
+        if (array[absValue - 1] < 0) return absValue;
+        array[absValue - 1] *= -1;
+    }
+}
