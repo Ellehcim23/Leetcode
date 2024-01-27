a24c08aea1ba9f127a6f54dc5a60f64d8e49f122
ellehcim burton <ellehcimburton@gmail.com>
2024-01-26 20:03:18 -0500

    feat: add solution to Validate BST

---
+++ LeetcodeQuestions/validateBst.js
@@ -0,0 +1,19 @@
+// O(n) time | O(d) space
+class BST {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+function validateBst(tree) {
+    return validateBstHelper(tree, -Infinity, Infinity)
+}
+
+function validateBstHelper(tree, min, max) {
+    if (tree === null) return true;
+    if (tree.value < min || tree.value >= max) return false;
+    leftIsValid = validateBstHelper(tree.left, min, tree.value);
+    return leftIsValid && validateBstHelper(tree.right, tree.value, max);
+}
